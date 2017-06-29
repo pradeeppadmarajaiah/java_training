aeb2b0631141fb1330ba8438b84e49a1f62c5a65
Pradeep Padmarajaiah <pradeep_p_13@in.ibm.com>
2017-06-29 11:10:29 +0530

    added inheritance with super

--- src/com/test/day9/CustomizedMap1.java
+++ src/com/test/day9/CustomizedMap1.java
@@ -5,19 +5,23 @@ public class CustomizedMap1 extends CustomizedMap {
 	public CustomizedMap1() {
 		System.out.println("This is CustomizedMap1");
 	}
+
 	public CustomizedMap1(int i) {
-		System.out.println("This is CustomizedMap1 with int argument" +i);
+		super(i);
+		System.out.println("This is CustomizedMap1 with int argument" + i);
 	}
+
 	@Override
 	public int sum(int input1, int input2) {
 		// TODO Auto-generated method stub
 		return (input1 - input2);
-		
+
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CustomizedMap1 customizedMap1 = new CustomizedMap1();
-		int sum= customizedMap1.sum(2, 3);
+		int sum = customizedMap1.sum(2, 3);
 		System.out.println(sum);
 		customizedMap1.createST();
 		CustomizedMap1 map1 = new CustomizedMap1(20);
